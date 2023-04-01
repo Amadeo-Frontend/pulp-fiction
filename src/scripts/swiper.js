3651dbc8f67951e2f6f6427f2a28ba1dbc037aed
Amadeo Bon <amadeosph@outlook.com>
2023-04-01 09:23:21 -0300

    fix:carousel

---
+++ src/scripts/swiper.js
@@ -0,0 +1,29 @@
+//SWIPER
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  autoplay: {
+    delay: 2500,
+  },
+  direction: "horizontal",
+  loop: true,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 30,
+    slideShadows: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
